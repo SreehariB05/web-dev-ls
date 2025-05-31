fc7e1f057d1b5021124a5a3d07da75047c00bc0f
SreehariB05 <sreehari.binoj@gmail.com>
2025-05-31 07:19:33 +0530

    moved useful shit to external js file

--- test1.js
+++ test1.js
@@ -1,1 +1,12 @@
 document.writeln("sadhanam kayyilundo?");
+console.log("kambilipputhapp");
+function rectArea(){
+	console.log(document.getElementById("length").value);
+	console.log(document.getElementById("breadth").value);
+    const a=document.getElementById("length").value;
+	const b=document.getElementById("breadth").value;
+	let c="";
+	c=parseInt(a)*parseInt(b);
+	console.log(c);
+	document.getElementById("area").innerText=`Area = ${c}`;
+}
